1178e4c6d105ddff26f0cceb0414afce3c26bc6f
csvinay <vinay.chappa@gmail.com>
2021-06-17 02:26:13 +0530

    final changes

--- edge/edge.py
+++ edge/edge.py
@@ -1,39 +1,64 @@
+# This is the publisher. The data is published to the broker through a specific topic
+
 import paho.mqtt.client as mqtt
 import time
 import csv
 import json
 import datetime
-
-def MQTT_publish(broker, file, topic):
-    def on_publish(client,userdata,result):             #create function for callback
-        print("data row:",count,"published")
-        pass
-    count = 0 
-    client = mqtt.Client()
-    print("Connecting to broker", broker)
-    client.connect(broker)
-    #client.loop_start()
-    print("Publishing...")
-    client.loop_start()
-    myfile = open(file, 'r')
-    reader = csv.reader(myfile)
-    next(reader)
-    mylist = list(reader)
-    myfile.close()
-    for line in mylist:
-        ts = time.time()
-        tst = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        line.append(tst)
-        strline = json.dumps(line)
-        count +=1
-        client.publish(f"{topic}", strline, qos=2, retain=True)
-        #counting the number of data rows published
-        client.on_publish = on_publish
-        time.sleep(1) 
-    client.loop_stop()
+import threading
 
 broker = "mqtt.eclipseprojects.io"
 file = "dataset.csv"
 topic="datarow"
-MQTT_publish(broker, file, topic)
 
+# To count the number of data rows transfere
+count = 0 
+#create function for callback whether the data is published or not
+def on_publish(client,userdata,result):             
+    print("data row:",count,"published")
+    pass
+
+#Creating a client instance
+client = mqtt.Client()
+
+#Connecting to the broker
+print("Connecting to broker", broker)
+client.connect(broker)
+
+#Function to publish the datarow to the server
+def publishing():
+    print("Publishing...")
+    client.loop_start()
+    #Reading the dataset.csv file
+    myfile = open(file, 'r')
+    reader = csv.reader(myfile)
+    next(reader) #Skipping the header
+    #Reading all the rows as list
+    mylist = list(reader)
+    myfile.close()
+    #Loop to read each row individually and publish it to the broker
+    for line in mylist:
+        global count
+        #Adding publishing time to the list
+        ts = time.time()
+        tst = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        line.append(tst)
+        strline = json.dumps(line)
+
+        #counting the number of data rows published
+        count +=1
+        
+        #Publishing the list
+        client.publish(f"{topic}", strline, qos=2, retain=True)
+        
+        #Giving an interval of 60 seconds between datarows transfer
+        time.sleep(5) 
+    client.loop_stop()
+
+#create function for callback whether the data is published or not
+def counting():
+    client.on_publish = on_publish
+
+#Multithreading to publish the data and count the datarows transfered simultaneously without any disturbance
+threading.Thread(target=counting()).start()
+threading.Thread(target=publishing()).start()
--- server/server.py
+++ server/server.py
@@ -1,25 +1,32 @@
+# This is the Subscriber. The publsihed data is subscribed from the broker through the specified topic
+
 import paho.mqtt.client as mqtt
 import time
 import csv
 from csv import writer
 import json
 import datetime
-# This is the Subscriber
 
 def on_message(client, userdata, message):
-    #print(str(message.payload.decode("utf-8")))
+    #Storing the datarow subscribed as string
     strrow = str(message.payload.decode("utf-8"))
+    #Converting string to list
     row = json.loads(strrow)
+    #Adding subscribed time to the data row
     ts = time.time()
     tst = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     row.append(tst)
     print("Receieved data successfully",row)
+    #Appending the datarow to the recvdataset.csv
     with open('recvdataset.csv', 'a') as f_object:
         writer_object = writer(f_object)
         writer_object.writerow(row)
         f_object.close()
 
+#Function to connect to the broker and subscribe the datarows
 def MQTT_subscribe(broker, topic):
+    #Unique client_id is given so that even if the process is broken and started again, 
+    # The previous published datarows can be retained and subscribed
     client = mqtt.Client(client_id="1259396904", clean_session=False)
     print("Connecting to broker", broker)
     client.connect(broker)
